1bfc56088fa13c49aa12f3270c7360b7412f81ad
Marco Romano <marcojulioromano@gmail.com>
2023-03-17 13:51:25 +0100

    Allow null backdrop
    Not all movies have a backdrop.

--- httpapi/public/src/main/kotlin/net/marcoromano/mooviez/httpapi/Movie.kt
+++ httpapi/public/src/main/kotlin/net/marcoromano/mooviez/httpapi/Movie.kt
@@ -7,7 +7,7 @@ public data class Movie(
   val id: Long,
   val title: String,
   val poster_path: String,
-  val backdrop_path: String,
+  val backdrop_path: String?,
   val overview: String,
   val vote_average: Double,
   val popularity: Double,
